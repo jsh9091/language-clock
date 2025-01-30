bca11fb9f5a6fe5c98a472f6d077e408a6a90cca
Joshua Horvath <jsh_197@hotmail.com>
2025-01-30 17:26:51 -0500

    Layout of Settings screen

--- language-clock/settings/index.jsx
+++ language-clock/settings/index.jsx
@@ -1,19 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Joshua Horvath
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 registerSettingsPage(({ settings }) => (
   <Page>
     <Section
       title={
-        <ColorSelect
-          settingsKey="color"
-          colors={[
-            { color: "aqua" },
-            { color: "red" },
-            { color: "gold" },
-            { color: "aquamarine" },
-            { color: "deepskyblue" },
-            { color: "plum" },
-          ]}
-        />
+        <Text bold align="center">
+          Language Clock Settings
+        </Text>
       }
-    ></Section>
+    >
+      <ColorSelect
+        settingsKey="color"
+        colors={[
+          { color: "aqua" },
+          { color: "red" },
+          { color: "gold" },
+          { color: "aquamarine" },
+          { color: "deepskyblue" },
+          { color: "plum" },
+        ]}
+      />
+      <Select
+        label={`Select Language`}
+        settingsKey="selection"
+        options={[
+          { name: "Hungarian" }, 
+          { name: "English" }, 
+          { name: "French" }, 
+          { name: "Spanish" }, 
+          { name: "German" }, 
+          { name: "Italian" }, 
+          { name: "Esperanto" }, 
+          { name: "Latin" }]}
+      />
+    </Section>
   </Page>
 ));
